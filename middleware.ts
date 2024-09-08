0671d6897a268609c87dd122821b0d3f854ab87a
MrCanonm <luis03301@hotmail.com>
2024-09-08 14:19:01 -0400

    change route

--- middleware.ts
+++ middleware.ts
@@ -5,18 +5,17 @@ export function middleware(req: NextRequest) {
   const token = req.cookies.get("authToken");
   const { pathname } = req.nextUrl;
 
-  if (!token && pathname !== "/pages/404") {
-    return NextResponse.redirect(new URL("/pages/404", req.url));
+  if (!token && pathname !== "/") {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
-  if (token && pathname === "/pages/404") {
-    return NextResponse.redirect(new URL("/pages/404", req.url));
+  if (token && pathname === "/") {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  // Skip all paths that should not be internationalized
-  matcher: ["/((?!_next|.*\\..*).*)"],
+  matcher: ["/", "/((?!/|api).{1,})"],
 };
